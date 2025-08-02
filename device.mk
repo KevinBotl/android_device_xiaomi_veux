e7de4c904ebf4710bdc177170bf126588105a8f9
Kevin <166707645+KevinBotl@users.noreply.github.com>
2025-08-02 19:47:43 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,6 +6,7 @@
 #
 
 LOCAL_PATH := device/xiaomi/veux
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -13,21 +14,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+# Boot control HAL & others
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.holi
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    android.hardware.boot@1.0-service \
     bootctrl.holi \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
